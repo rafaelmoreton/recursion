02b3c56909a24cee46e28dd1a73dec98d3309794
Rafael Moreton Baggio <rafaelmoreton@gmail.com>
2021-12-17 08:25:37 -0300

    Fibonacci conclusion: Patched #fibs_recursive to return the same list #fibs returns. Refactored #fibs to be more concise and readable.

--- recursion.rb
+++ recursion.rb
@@ -1,25 +1,19 @@
+# frozen_string_literal: true
+
 def fibs(number)
-    fibo_sequence = []
-    i = 0
-    until i == number do
-        case i
-        when 0 then fibo_sequence << 0
-        when 1 then fibo_sequence << 1
-        else
-            fibo_sequence << (fibo_sequence[i - 1] + fibo_sequence[i - 2])
-        end
-        i += 1
-    end
-    fibo_sequence
+  fibo_sequence = []
+  i = 0
+  until i == number
+    fibo_sequence << (i <= 1 ? i : fibo_sequence[i - 1] + fibo_sequence[i - 2])
+    i += 1
+  end
+  fibo_sequence
 end
 
-def fibs_recursive(number, fibo_sequence = [])
-        if number < 2
-            fibo_sequence << number 
-        else
-            fibo_sequence << (fibs_recursive(number - 1) + fibs_recursive(number - 2))
-        end
-    fibo_sequence
-end
+def fibs_recursive(number)
+  return [0] if number == 1
+  return [0, 1] if number == 2
 
-p fibs_recursive(1)
+  array = fibs_recursive(number - 1)
+  array << array[number - 3] + array[number - 2]
+end
